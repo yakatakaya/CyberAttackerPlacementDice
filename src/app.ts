b4f5e221fa4f1d284bf99445b1f0441681c56913
yakata <tagat92tagat@gmail.com>
2023-10-14 17:38:03 +0900

    一旦完成？

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
 import "@babylonjs/loaders/OBJ";
 
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, GroundMesh,PhysicsAggregate,PhysicsShapeType,HavokPlugin, TransformNode } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, GroundMesh,PhysicsAggregate,PhysicsShapeType,HavokPlugin, TransformNode, ActionManager,ExecuteCodeAction } from "@babylonjs/core";
 import { PhysicsImpostor, PhysicsShapeContainer, PhysicsShapeMesh,PhysicsBody, PhysicsMotionType } from '@babylonjs/core/Physics';
 
 import HavokPhysics, { HavokPhysicsWithBindings } from "@babylonjs/havok";
@@ -21,6 +21,10 @@ class App {
         const app = new App(havok);
         return 
     }
+    static getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
     constructor(havok : HavokPhysicsWithBindings) {
         // create the canvas html element and attach it to the webpage
         var canvas = document.createElement("canvas");
@@ -33,13 +37,10 @@ class App {
         var engine = new Engine(canvas, true);
         var scene = new Scene(engine);
         scene.enablePhysics(new Vector3(0, -9.81, 0), new HavokPlugin(true, havok));
-
         var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 4, Math.PI / 6, 20, Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
-
         const ground = MeshBuilder.CreateGround("ground", {height: 20, width: 20, subdivisions: 4});
         new PhysicsAggregate(ground, PhysicsShapeType.BOX, {mass: 0, friction: 0.7, restitution: 0.8}, scene);
-
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
         
         SceneLoader.ImportMeshAsync("", "./models/", "CyberAttackerPlacement_4SidedDice.glb", scene)
@@ -47,20 +48,33 @@ class App {
             let root = new TransformNode("root");
             let boxClone2Shape = new PhysicsShapeContainer(scene);
             root.position.set(3, 3, 0);
-    
+            var body = new PhysicsBody(root, PhysicsMotionType.DYNAMIC, false, scene);
+            
+            var applyForce = function (physicsBody : PhysicsBody, obj : TransformNode, vector :Vector3) {
+                var point = obj.absolutePosition;
+                point.y -= 1;
+
+                physicsBody.applyForce(
+                    vector,
+                    point
+                );
+            }
 
             // Mesh shape
             result.meshes.forEach((mesh)=>{
                 mesh.parent = root;
                 let phy = new PhysicsShapeMesh(mesh as Mesh, scene);
                 boxClone2Shape.addChildFromParent(root, phy, mesh);
+                mesh.actionManager = new ActionManager(scene);
+                mesh.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnPickUpTrigger, function () {
+                    applyForce(body, root, new Vector3(App.getRandomInt(100), 1000, App.getRandomInt(100)));
+                }));
             });
 
-            var body = new PhysicsBody(root, PhysicsMotionType.DYNAMIC, false, scene);
 
             body.shape = boxClone2Shape;
             body.setMassProperties ({
-                mass: 1,
+                mass: 2,
             });
                     
         });
@@ -69,20 +83,32 @@ class App {
             let root = new TransformNode("root");
             let boxClone2Shape = new PhysicsShapeContainer(scene);
             root.position.set(-3, 3, 0);
-    
+            var body = new PhysicsBody(root, PhysicsMotionType.DYNAMIC, false, scene);
+
+            var applyForce = function (physicsBody : PhysicsBody, obj : TransformNode, vector :Vector3) {
+                var point = obj.absolutePosition;
+                point.y -= 1;
+
+                physicsBody.applyForce(
+                    vector,
+                    point
+                );
+            }
 
             // Mesh shape
             result.meshes.forEach((mesh)=>{
                 mesh.parent = root;
                 let phy = new PhysicsShapeMesh(mesh as Mesh, scene);
                 boxClone2Shape.addChildFromParent(root, phy, mesh);
+                mesh.actionManager = new ActionManager(scene);
+                mesh.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnPickUpTrigger, function () {
+                    applyForce(body, root, new Vector3(App.getRandomInt(100), 1000, App.getRandomInt(100)));
+                }));
             });
-
-            var body = new PhysicsBody(root, PhysicsMotionType.DYNAMIC, false, scene);
 
             body.shape = boxClone2Shape;
             body.setMassProperties ({
-                mass: 1,
+                mass: 2,
             });
 
         });
@@ -91,20 +117,33 @@ class App {
             let root = new TransformNode("root");
             let boxClone2Shape = new PhysicsShapeContainer(scene);
             root.position.set(0, 3, 0);
-    
+            root.scaling.scaleInPlace(0.5);
+
+            var body = new PhysicsBody(root, PhysicsMotionType.DYNAMIC, false, scene);
+
+            var applyForce = function (physicsBody : PhysicsBody, obj : TransformNode, vector :Vector3) {
+                var point = obj.absolutePosition;
+                point.y -= 1;
+
+                physicsBody.applyForce(
+                    vector,
+                    point
+                );
+            }
 
             // Mesh shape
             result.meshes.forEach((mesh)=>{
                 mesh.parent = root;
                 let phy = new PhysicsShapeMesh(mesh as Mesh, scene);
                 boxClone2Shape.addChildFromParent(root, phy, mesh);
+                mesh.actionManager = new ActionManager(scene);
+                mesh.actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnPickUpTrigger, function () {
+                    applyForce(body, root, new Vector3(App.getRandomInt(100), 1000, App.getRandomInt(100)));
+                }));
             });
-
-            var body = new PhysicsBody(root, PhysicsMotionType.DYNAMIC, false, scene);
-
             body.shape = boxClone2Shape;
             body.setMassProperties ({
-                mass: 1,
+                mass: 2,
             });
 
         });
